36af66206a855a2e28892e06728d25a0f47087fa
tinhtq <truongquangtinh1997@gmail.com>
2023-08-25 14:51:23 +0700

    update athena query

---
+++ dop-c02/s3-athena/athena-query.sql
@@ -0,0 +1,36 @@
+create database s3_access_logs_db
+CREATE EXTERNAL TABLE `s3_access_logs_db.mybucket_logs`(
+  `bucketowner` STRING,
+  `bucket_name` STRING,
+  `requestdatetime` STRING,
+  `remoteip` STRING,
+  `requester` STRING,
+  `requestid` STRING,
+  `operation` STRING,
+  `key` STRING,
+  `request_uri` STRING,
+  `httpstatus` STRING,
+  `errorcode` STRING,
+  `bytessent` BIGINT,
+  `objectsize` BIGINT,
+  `totaltime` STRING,
+  `turnaroundtime` STRING,
+  `referrer` STRING,
+  `useragent` STRING,
+  `versionid` STRING,
+  `hostid` STRING,
+  `sigv` STRING,
+  `ciphersuite` STRING,
+  `authtype` STRING,
+  `endpoint` STRING,
+  `tlsversion` STRING)
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.serde2.RegexSerDe'
+WITH SERDEPROPERTIES (
+  'input.regex'='([^ ]*) ([^ ]*) \\[(.*?)\\] ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) (\"[^\"]*\"|-) (-|[0-9]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) (\"[^\"]*\"|-) ([^ ]*)(?: ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*))?.*$')
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.mapred.TextInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://pipeline-artifact-bucket-601619162398/reports/'
