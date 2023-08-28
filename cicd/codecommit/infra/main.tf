ee3a57bf67a837a89af8fac4354c2ad51459a322
tinhtq <truongquangtinh1997@gmail.com>
2023-08-28 16:25:37 +0700

    add cloudwatch event rule

--- cicd/codecommit/infra/main.tf
+++ cicd/codecommit/infra/main.tf
@@ -1,7 +1,8 @@
 
 resource "aws_codecommit_repository" "test" {
-  repository_name = "MyTestRepo"
+  repository_name = var.repository_name
   description     = "The repository for static website"
+  default_branch = var.branch
   tags = {
     Environment = "test"
   }
@@ -9,4 +10,12 @@ resource "aws_codecommit_repository" "test" {
 
 output "repository_clone_http" {
   value = aws_codecommit_repository.test.clone_url_http
+}
+
+variable "repository_name" {
+  default = "MyTestRepo"
+}
+
+variable "branch" {
+  default = "main"
 }
