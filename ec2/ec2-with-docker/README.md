b559c3ae9b00ede8cb101987570c874bac29de70
tinhtq <truongquangtinh1997@gmail.com>
2023-10-12 14:12:40 +0700

    update ec2 with docker in kodekloud

---
+++ ec2/ec2-with-docker/README.md
@@ -0,0 +1,3 @@
+# Create EC2 with Docker installed
+
+It can make easy to deploy EC2 in Kodekloud.
---
+++ ec2/ec2-with-docker/main.tf
@@ -0,0 +1,45 @@
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_ami" "amazon-ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+resource "aws_security_group" "instance" {
+  name = "security-group-test"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_instance" "example" {
+  key_name               = aws_key_pair.example.key_name
+  ami                    = data.aws_ami.amazon-ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = data.template_file.userdata.rendered
+}
+data "template_file" "userdata" {
+  template = file("${path.module}/userdata.tpl")
+}
