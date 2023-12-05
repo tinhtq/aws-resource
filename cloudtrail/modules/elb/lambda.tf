8e3c1538e29a7c799e8630c4f3af4e901dc60d46
tinhtq <truongquangtinh1997@gmail.com>
2023-12-05 19:08:59 +0700

    add cloudtrail

---
+++ cloudtrail/modules/elb/lambda.tf
@@ -0,0 +1,59 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "cloudtrail-lambda-role-${var.resources}-${var.region}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_iam_role_policy_attachment" "elb_fullaccess" {
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "${path.module}/files/"
+  output_path = "${path.module}/files/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "auto_create_tag" {
+  filename      = "${path.module}/files/lambda_function_payload.zip"
+  function_name = "auto_create_tags_${var.resources}"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main.lambda_handler"
+  environment {
+    variables = {
+      REGION = var.region
+    }
+  }
+  timeout          = 600
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.10"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.auto_create_tag.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.create_resources.arn
+}
---
+++ cloudtrail/modules/elb/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "resources" {
+  default = "elb"
+}
