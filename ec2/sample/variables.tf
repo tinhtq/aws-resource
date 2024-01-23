e43b266db7484cb3752cfe517c2f54a061afd458
tinhtq <truongquangtinh1997@gmail.com>
2024-01-23 16:12:43 +0700

    update redhat

--- ec2/sample/variables.tf
+++ ec2/sample/variables.tf
@@ -3,14 +3,17 @@ variable "number_ec2_instance" {
 }
 # Ubuntu:  *ubuntu-jammy-22.04-amd64-server-*
 # Fedora  fedora-coreos-38*-x86_64
+# Fedora Fedora-Cloud-Base-37*x86_64*
 # CentOS centos7-hvm-x86_64
+# RedHat RHEL-9.3.0*x86_64*
 variable "ami_filter" {
-  default = "*centos7-hvm-x86_64*"
+  default = "RHEL-9.3.0*x86_64*"
 }
 
 # Ubuntu: amazon
 # Fedora: 125523088429
 # CentOs: 247102896272
+# RedHat: 309956199498
 variable "ami_owner" {
-  default = "247102896272"
+  default = "309956199498"
 }
