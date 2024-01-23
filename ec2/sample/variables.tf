7dea1b9ccc271e3bc3c6dad5c5c995e1e1ffadb2
tinhtq <truongquangtinh1997@gmail.com>
2024-01-23 16:45:40 +0700

    update amazon linux

--- ec2/sample/variables.tf
+++ ec2/sample/variables.tf
@@ -1,19 +1,20 @@
 variable "number_ec2_instance" {
   default = 3
 }
+# Amazon Linux: al2023-ami-*-x86_64
 # Ubuntu:  *ubuntu-jammy-22.04-amd64-server-*
 # Fedora  fedora-coreos-38*-x86_64
 # Fedora Fedora-Cloud-Base-37*x86_64*
 # CentOS centos7-hvm-x86_64
 # RedHat RHEL-9.3.0*x86_64*
 variable "ami_filter" {
-  default = "RHEL-9.3.0*x86_64*"
+  default = "al2023-ami-2023*-x86_64"
 }
-
+# Amazon Linux: 
 # Ubuntu: amazon
 # Fedora: 125523088429
 # CentOs: 247102896272
 # RedHat: 309956199498
 variable "ami_owner" {
-  default = "309956199498"
+  default = "amazon"
 }
