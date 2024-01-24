a6fc8f724a9f731cd7bbe6a49f2f05c7f7a417f8
tinhtq <truongquangtinh1997@gmail.com>
2024-01-24 11:19:46 +0700

    update terraform modules

--- ec2/sample/variables.tf
+++ ec2/sample/variables.tf
@@ -1,20 +1,25 @@
-variable "number_ec2_instance" {
-  default = 1
+variable "instances" {
+  default = [
+    { quantity = 1, ami_filter = "al2023-ami-*-x86_64", ami_owner = "amazon" },
+    { quantity = 0, ami_filter = "*ubuntu-jammy-22.04-amd64-server-*", ami_owner = "amazon" },
+    { quantity = 4, ami_filter = "fedora-coreos-38*-x86_64", ami_owner = "125523088429" },
+  ]
 }
+
 # Amazon Linux: al2023-ami-*-x86_64
 # Ubuntu:  *ubuntu-jammy-22.04-amd64-server-*
 # Fedora  fedora-coreos-38*-x86_64
 # Fedora Fedora-Cloud-Base-37*x86_64*
 # CentOS centos7-hvm-x86_64
 # RedHat RHEL-9.3.0*x86_64*
-variable "ami_filter" {
-  default = "*ubuntu-jammy-22.04-amd64-server-*"
-}
+# variable "ami_filter" {
+#   default = "*ubuntu-jammy-22.04-amd64-server-*"
+# }
 # Amazon Linux: amazon
 # Ubuntu: amazon
 # Fedora: 125523088429
 # CentOs: 247102896272
 # RedHat: 309956199498
-variable "ami_owner" {
-  default = "amazon"
-}
+# variable "ami_owner" {
+#   default = "amazon"
+# }
