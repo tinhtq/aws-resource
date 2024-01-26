93d8d053888f902e2b073abba35bc724ca2a3dc3
tinhtq <truongquangtinh1997@gmail.com>
2024-01-26 13:39:06 +0700

    update policy

--- eks/terraform/data.tf
+++ eks/terraform/data.tf
@@ -3,12 +3,13 @@ data "aws_vpc" "default" {
 }
 
 data "aws_subnet" "zone_a" {
-  vpc_id = data.aws_vpc.default.id
+  vpc_id            = data.aws_vpc.default.id
   availability_zone = "us-east-1a"
-  default_for_az = true
+  default_for_az    = true
 }
+
 data "aws_subnet" "zone_b" {
-  vpc_id = data.aws_vpc.default.id
+  vpc_id            = data.aws_vpc.default.id
   availability_zone = "us-east-1b"
-  default_for_az = true
+  default_for_az    = true
 }
