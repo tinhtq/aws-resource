27e654329ad2906adc105018047bbcc8a35cea92
tinhtq <truongquangtinh1997@gmail.com>
2024-10-26 22:57:05 +0700

    update eks terraform

---
+++ eks/terraform/iam.tf
@@ -0,0 +1,22 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks" {
+  name_prefix        = "eks-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
