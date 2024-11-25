5d29b1a43f78f0c519e321a7d440a476b07b9a13
tinhtq <truongquangtinh1997@gmail.com>
2024-11-25 10:45:14 +0700

    update list email

--- rds/promote-read-replicas/cloudwatch.tf
+++ rds/promote-read-replicas/cloudwatch.tf
@@ -1,20 +1,3 @@
-resource "aws_cloudwatch_log_group" "rds_logs" {
-  name = "/aws/rds/mydb/logs"
-}
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "HighCPUUtilization"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 5
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/RDS"
-  period              = 300
-  statistic           = "Average"
-  threshold           = 80
-
-  dimensions = {
-    DBInstanceIdentifier = aws_rds_cluster.primary.id
-  }
-
-  alarm_description = "Triggers if CPU utilization exceeds 80% for 5 minutes"
+resource "aws_cloudwatch_log_group" "rds" {
+  name = "/aws/rds/cluster/${var.rds_cluster_name}/error"
 }
