bbf297620e527cb08beb70973e776cdde1012424
tinhtq <truongquangtinh1997@gmail.com>
2024-12-20 10:00:09 +0700

    update lambda function

--- apigw/apigw-hello-world-lambda/lambda.tf
+++ apigw/apigw-hello-world-lambda/lambda.tf
@@ -29,25 +29,20 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 resource "aws_iam_role" "lambda_execution_role" {
-  name               = "lambda-role"
+  name               = "lambda-execution-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaKinesisExecutionRole"
-  role       = aws_iam_role.iam_for_lambda.name
 }
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_dir  = "${path.module}/files/lambda/"
-  output_path = "${path.module}/files/lambda_function_payload.zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/lambda_function_payload.zip"
 }
 
 resource "aws_lambda_function" "get_data_kinesis" {
-  filename      = "${path.module}/files/lambda_function_payload.zip"
-  function_name = "get-data-kinesis"
-  role          = aws_iam_role.iam_for_lambda.arn
+  filename      = "${path.module}/lambda_function_payload.zip"
+  function_name = "hello-world"
+  role          = aws_iam_role.lambda_execution_role.arn
   handler       = "main.lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
@@ -55,8 +50,3 @@ resource "aws_lambda_function" "get_data_kinesis" {
   runtime = "python3.10"
 }
 
-resource "aws_lambda_event_source_mapping" "kinesis" {
-  event_source_arn  = aws_kinesis_stream.stream.arn
-  function_name     = aws_lambda_function.get_data_kinesis.arn
-  starting_position = "LATEST"
-}
