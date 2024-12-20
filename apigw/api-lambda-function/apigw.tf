bbabc6d561229ff722d595d1ef191b6c51a19e43
tinhtq <truongquangtinh1997@gmail.com>
2024-12-20 09:47:18 +0700

    update proxy api gw

--- apigw/api-lambda-function/apigw.tf
+++ apigw/api-lambda-function/apigw.tf
@@ -1,27 +1,10 @@
 resource "aws_api_gateway_rest_api" "hello_world" {
   name        = "basic-invoke-lambda-function"
-  description = "API Gateway for Lambda example"
+  description = "API Gateway for Hello World Lambda"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
-  parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.hello_world.id
+  parent_id   = aws_api_gateway_rest_api.hello_world.root_resource_id
   path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.example_api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.example_lambda.invoke_arn
 }
